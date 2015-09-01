4a077384b6004aa64210c0c08bfbc874bfbc8616
Matt Eves <mattheweves@outlook.com>
2015-09-01 02:02:13 +0000

    Lesson 12

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Flixster::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   root 'static_pages#index'
-
+  namespace :instructor do
+    resources :courses, :only => [:new, :create, :show]
+  end
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
