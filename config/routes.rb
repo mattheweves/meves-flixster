47db39cab0ade3121adc769a13de6deec377d60e
Matt Eves <mattheweves@outlook.com>
2015-09-02 14:07:48 +0000

    Lesson 20

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Flixster::Application.routes.draw do
 
   root 'static_pages#index'
   resources :courses, :only => [:index, :show]
+  resources :lessons, :only => [:show]
   namespace :instructor do
     resources :sections, :only => [] do
       resources :lessons, :only => [:new, :create]
