07099618c0553509fae226fbfa780bacaf2ed510
Matt Eves <mattheweves@outlook.com>
2015-09-04 03:31:52 +0000

    Lesson 27

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,6 +1,9 @@
 class LessonsController < ApplicationController
-	
+	before_action :authenticate_user!
+	before_action :require_enrollment_for_current_course, :only => [:show]
+
 	def show
+
 	end
 
 private
@@ -8,6 +11,12 @@ private
 helper_method :current_lesson
 def current_lesson
 	@current_lesson ||= Lesson.find(params[:id])
+end
+
+def require_enrollment_for_current_course
+	if !current_user.enrolled_in?(current_lesson.section.course)
+	   redirect_to course_path(current_lesson.section.course), :alert => 'You must be enrolled in the course to view the associated lessons.'
+	end
 end
 
 
